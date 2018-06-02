9dbb565506fe75ad42f537d49b03236ae20f61b4
alfre <alfre@Alfredo-PC>
2018-06-02 04:56:12 +0200

    css

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="card-effect.css"/>
         <link rel="stylesheet" type="text/css" href="style.css"/>
-        
+
         <link rel="icon" href=""/>
         <!-- ===== ===== CSS ===== ===== -->
         <!-- ===== ===== ===== ===== ===== ===== ===== -->
@@ -65,111 +65,14 @@
                 <!-- ===== ===== ===== ===== ===== ===== ===== -->
                 <article class="row">
                     <article id="card" class="col-lg-3 col-md-6 col-sm-6 col-11 mx-auto mt-3 p-2">
-                        <div class="tarjeta">
-                            <div class="front">
+                        <div class="tarjeta row">
+                            <div class="front col-12">
                                 <div class="card-img-top">
                                 </div>
-                                <div class="card-body d-none d-md-flex">
-                                    <h6 class="card-title text-center">Maestr&iacute;a en Eficiencia Energ&eacute;tica y energ&iacute;as Renovables</h6>
-                                </div>
-                                <div class="card-body d-md-none">
-                                    <h5 class="card-title col-12 mt-4 mb-3 p-0 pl-1 pr-1 text-center">Maestr&iacute;a en Eficiencia Energ&eacute;tica y energ&iacute;as Renovables</h5>
-                                    <div class="col-12 mb-2 mt-4 ml-1">
-                                        <div class="row">
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">watch_later</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">87 Créditos</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">school</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Modalidad Online</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">star</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Beca del 50%</span>
-                                        </div>
-                                    </div>
-                                    <span class="col-12 mt-4">
-                                        <div class="row">
-                                            <a data-toggle="modal" data-target="#form" class="col-10 mx-auto btn btn-primary font-weight-bold curso">M&aacute;s informaci&oacute;n</a>
-                                            <input class="d-none" value="MAESTRÍA EN EFICIENCIA ENERGÉTICA Y ENERGÍAS RENOVABLES">
-                                        </div>
-                                    </span>
-                                </div>
                             </div>
-                            <div class="back ">
+                            <div class="back col-12 ">
                                 <div class="card-back row p-2">
-                                    <h5 class="card-title col-12 mt-4 mb-3 p-0 pl-1 pr-1 text-center">Maestr&iacute;a en Eficiencia Energ&eacute;tica y energ&iacute;as Renovables</h5>
-                                    <div class="col-12 mb-2 mt-4 ml-1">
-                                        <div class="row">
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">watch_later</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">87 Créditos</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">school</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Modalidad Online</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">star</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Beca del 50%</span>
-                                        </div>
-                                    </div>
-                                    <span class="col-12 mt-4">
-                                        <div class="row">
-                                            <a data-toggle="modal" data-target="#form" class="col-10 mx-auto btn btn-primary font-weight-bold curso">M&aacute;s informaci&oacute;n</a>
-                                            <input class="d-none" value="MAESTRÍA EN EFICIENCIA ENERGÉTICA Y ENERGÍAS RENOVABLES">
-                                        </div>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article id="card" class="col-lg-3 col-md-6 col-sm-6 col-11 mx-auto mt-3 p-2">
-                        <div class="tarjeta">
-                            <div class="front">
-                                <div class="card-img-top">
-                                </div>
-                                <div class="card-body d-none d-md-flex">
-                                    <h6 class="card-title text-center">Maestr&iacute;a en Community Manager y Posicionamiento Web</h6>
-                                </div>
-                                <div class="card-body d-md-none">
-                                    <h5 class="card-title col-12 mt-4 mb-3 p-0 pl-1 pr-1 text-center">Maestr&iacute;a en Community Manager y Posicionamiento Web</h5>
-                                    <div class="col-12 mb-2 mt-4 ml-1">
-                                        <div class="row">
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">watch_later</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">87 Créditos</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">school</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Modalidad Online</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">star</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Beca del 50%</span>
-                                        </div>
-                                    </div>
-                                    <span class="col-12 mt-4">
-                                        <div class="row">
-                                            <a data-toggle="modal" data-target="#form" class="col-10 mx-auto btn btn-primary font-weight-bold curso">M&aacute;s informaci&oacute;n</a>
-                                            <input class="d-none" value="MAESTRÍA EN COMMUNITY MANAGER Y POSICIONAMIENTO WEB">
-                                        </div>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="back ">
-                                <div class="card-back row p-2">
-                                    <h5 class="card-title col-12 mt-4 mb-3 p-0 pl-1 pr-1 text-center">Maestr&iacute;a en Community Manager y Posicionamiento Web</h5>
-                                    <div class="col-12 mb-2 mt-4 ml-1">
-                                        <div class="row">
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">watch_later</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">87 Créditos</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">school</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Modalidad Online</span>
-                                            <span class="col-12"></span>
-                                            <span class="material-icons col-1 pl-2 pr-0 ml-auto">star</span>
-                                            <span class="col-10 text-left pr-0 mr-auto">Beca del 50%</span>
-                                        </div>
-                                    </div>
-                                    <span class="col-12 mt-4">
-                                        <div class="row">
-                                            <a data-toggle="modal" data-target="#form" class="col-10 mx-auto btn btn-primary font-weight-bold curso">M&aacute;s informaci&oacute;n</a>
-                                            <input class="d-none" value="MAESTRÍA EN COMMUNITY MANAGER Y POSICIONAMIENTO WEB">
-                                        </div>
-                                    </span>
+                                    <a class="col-auto mx-auto btn btn-primary font-weight-bold curso">Jugar</a>
                                 </div>
                             </div>
                         </div>
@@ -180,7 +83,7 @@
                 <article class="row ">
                     <!--===== ===== ===== ===== ===== ===== =====--> 
                     <!--===== ===== COPYGATE ===== =====--> 
-                    <section class="footer col-6 ml-auto mr-auto mt-4 text-center small ">
+                    <section class="footer col-6 ml-auto mr-auto mt-4 text-center small fixed-bottom">
                         <span>Copygate &Psi; 2018 www.didactical-games.com</span>
                     </section>
                     <!--===== ===== COPYGATE ===== =====--> 
